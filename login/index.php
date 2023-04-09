131adc78c8530fda1bd837fbf6491f4d2d11410f
Aman singh <118040115+amansingh331@users.noreply.github.com>
2023-04-09 15:56:06 +0530

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,34 @@
+<div id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-offset-2 col-md-8 col-sm-12">
+                <h2>LOGIN</h2>
+
+
+                <div class="alert alert-danger text-center" role="alert">
+                    <strong>Hey!</strong> Only club members have access to this.
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+
+
+
+            </div>
+            <div class="col-md-offset-3 col-md-6 col-sm-8 mt30 ">
+                <form action="index.html" method="post" role="form">
+                    <div class="col-md-12 col-sm-6">
+                        <label for="name">EMAIL</label>
+                        <input name="name" type="email" class="form-control" id="email" value="" required>
+                        <label for="email">PASSWORD</label>
+                        <input name="email" type="password" class="form-control" id="password" required>
+                    </div>
+
+                    <div class="col-md-12 col-sm-12">
+                        <button type="submit" name="login" value="login" class="btn btn-default">Login</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ login/welcome.php
@@ -0,0 +1,12 @@
+<div id="contact">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-offset-2 col-md-8 col-sm-12">
+				<h2>welcome to the dashboard</h2>
+			</div>
+			
+			
+		</div>
+	</div>
+</div>		
+
